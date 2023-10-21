1f40555d8190a604207ef80112808be011dae80c
keleios <brendan.geiger@gmail.com>
2023-10-20 22:21:13 -0700

    Added Premake

---
+++ premake5.lua
@@ -0,0 +1,52 @@
+workspace "sfml_project"
+	configurations { "Debug", "Release" }
+	location "build"
+
+project "sfml_project"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir "bin/%{cfg.buildcfg}"
+
+	files { "src/**.*" }
+
+	filter "configurations:*"
+		defines { "SFML_STATIC" }
+		includedirs { "dependencies/SFML/include" }
+		libdirs { "dependencies/SFML/lib" }
+		links
+		{
+			"opengl32",
+			"freetype",
+			"winmm",
+			"gdi32",
+			"flac",
+			"vorbisenc",
+			"vorbisfile",
+			"vorbis",
+			"ogg",
+			"ws2_32"
+		}
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+		links
+		{	
+			"sfml-graphics-s-d",
+			"sfml-window-s-d",
+			"sfml-system-s-d",
+			"sfml-audio-s-d",
+			"sfml-network-s-d"
+		}
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
+		links
+		{	
+			"sfml-graphics-s",
+			"sfml-window-s",
+			"sfml-system-s",
+			"sfml-audio-s",
+			"sfml-network-s"
+		}
